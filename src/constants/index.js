e74e6ff9aca642c47da5b2b45480d7dd96b4bd1e
CDDSINSA <calidad.datossns@gmail.com>
2026-07-10 16:17:01 -0600

    Delete src directory

--- src/constants/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./promoConstants";
-export * from "./permissions";
-export * from "./seedData";
-export * from "./sheets";
-export * from "./ui";
--- src/context/AuthContext.jsx
+++
@@ -1,7 +0,0 @@
-import React, { createContext } from "react";
-
-export const AuthContext = createContext(null);
-
-export function AuthProvider({ value, children }) {
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-}
